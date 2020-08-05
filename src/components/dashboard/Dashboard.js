bb6ab47798691f207fe8e794c19852515ff82682
Jakov1970 <45370614+Jakov1970@users.noreply.github.com>
2020-08-05 19:20:22 +0100

    syncing data with firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase' //treba nam za konekciju na bazu kako bi se izlistali svi projekti iz baze na stranici
+import { compose } from 'redux'
 
 class Dashboard extends Component {
     render() {
@@ -23,9 +25,18 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+    console.log(state);
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects //ovo smo promenili. Sada pristupa bazi, ne koristi vise dummy data
     }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'projects'} //ovde mu kazemo koju kolekciju zeli da uzme. Uvek ce da update-uje state pri svakoj promeni jer ce to da vidi
+    ])
+)(Dashboard)
+
+//umesto export default connect(mapStateToProps)(Dashboard)
+//koristimo compose da bi konektovali dve funkcije
