631adec73aa5295b33dbd1283b9c471cf87f6c7f
Jakov1970 <45370614+Jakov1970@users.noreply.github.com>
2020-08-08 16:06:07 +0100

    logIn Added

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const initState = {} //ovde pravimo pocetno stanje koje prosledjujemo state-u jer na pocetku nemamo podatke za state, ali mora da se prosledi makar i prazan objekat zbog f-je ispod
+const initState = {
+    authReducer: null
+} //ovde pravimo pocetno stanje koje prosledjujemo state-u jer na pocetku nemamo podatke za state, ali mora da se prosledi makar i prazan objekat zbog f-je ispod
 
-const authReducer = (state = initState, actions) => {
-    return state
+const authReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'LOGIN_ERROR': 
+        console.log('Login error')
+        return {
+            ...state,
+            authError: 'Login failed'
+        }
+        case 'LOGIN_SUCCESS':
+            console.log('login success')
+            return{
+                ...state,
+                authError: null
+            }
+        default: 
+            return state;
+    }
 }
 
 export default authReducer
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import {combineReducers} from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
-    firestore: firestoreReducer //syncing data with firestore
+    firestore: firestoreReducer, //syncing data with firestore(bazom)
+    firebase: firebaseReducer   //za auth za korisnika
 })
 
 export default rootReducer
